23c49b78577d5f413b9de7cafab22b7235eee143
Joshua <legodude17@users.noreply.github.com>
2021-06-03 20:20:17 -0700

    Fix NRE due to missing ApparelTracker

--- Source/VAE Accessories/VAE Accessories/HarmonyPatches.cs
+++ Source/VAE Accessories/VAE Accessories/HarmonyPatches.cs
@@ -23,28 +23,28 @@ namespace VAE_Accessories
 
         public static void ApparelMassCapacity(Pawn p, StringBuilder explanation, ref float __result)
         {
-            if (!p.apparel.WornApparel.NullOrEmpty() &&
+            if (!(p?.apparel?.WornApparel.NullOrEmpty() ?? true) &&
                 p.apparel.WornApparel.FirstOrDefault(a => a.def is CaravanCapacityApparelDef)?.def is
                     CaravanCapacityApparelDef def) __result += def.carryingCapacity;
         }
 
         public static bool BeltsOnDeath(DamageInfo? dinfo, Hediff exactCulprit, Pawn __instance)
         {
-            if (!__instance.apparel.WornApparel.NullOrEmpty())
-                for (var i = __instance.apparel.WornApparel.Count - 1; i >= 0; --i)
+            if (__instance?.apparel?.WornApparel.NullOrEmpty() ?? true) return true;
+            for (var i = __instance.apparel.WornApparel.Count - 1; i >= 0; --i)
+            {
+                var apparel = __instance.apparel.WornApparel[i];
+                if (apparel.TryGetComp<CompExplodeOnDeath>() is CompExplodeOnDeath comp)
                 {
-                    var apparel = __instance.apparel.WornApparel[i];
-                    if (apparel.TryGetComp<CompExplodeOnDeath>() is CompExplodeOnDeath comp)
-                    {
-                        comp.ExplodeOnDeath(__instance);
-                    }
-                    else if (apparel is ResurrectorBelt)
-                    {
-                        UtilityMethods.ResurrectBeforeDeath(__instance);
-                        __instance.apparel.Remove(apparel);
-                        return false;
-                    }
+                    comp.ExplodeOnDeath(__instance);
                 }
+                else if (apparel is ResurrectorBelt)
+                {
+                    UtilityMethods.ResurrectBeforeDeath(__instance);
+                    __instance.apparel.Remove(apparel);
+                    return false;
+                }
+            }
 
             return true;
         }
